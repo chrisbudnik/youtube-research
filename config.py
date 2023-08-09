14a8fb1b50d8232f913e3a52dcce7a45a37169bb
chrisb <chrism.budnik@gmail.com>
2023-08-09 21:20:18 +0200

    update config, add setup note

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-#TO-DO add dummy keys
 class Config:
-    YOUTUBE_API_SERVICE_NAME = ""
-    YOUTUBE_API_VERSION = ""
-    DEVELOPER_KEY = ""
-    BQ_SERVICE_ACCOUNT_KEYS_PATH = ""
+    """
+    Set-up: add your youtube api key and path to bigquery service account keys (json file).
+    """
+    YOUTUBE_API_SERVICE_NAME = "youtube"
+    YOUTUBE_API_VERSION = "v3"
+    DEVELOPER_KEY = "YOUTUBE-API-KEY"
+    BQ_SERVICE_ACCOUNT_KEYS_PATH = "PATH-TO-KEYS"
